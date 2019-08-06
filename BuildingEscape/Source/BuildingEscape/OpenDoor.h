c58f26c768147b70471e53032e3a3160455bc7f5
Abin Alex Pothen <abinalexpothen@gmail.com>
2019-08-06 17:57:27 -0400

    Used the door open C++ event in blueprint.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -7,6 +7,7 @@
 #include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpenRequest);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
@@ -27,6 +28,9 @@ public:
     
     void OpenDoor();
     void CloseDoor();
+    
+    UPROPERTY(BlueprintAssignable)
+    FOnOpenRequest OnOpenRequest;
 
 private:
     UPROPERTY(EditAnywhere)
