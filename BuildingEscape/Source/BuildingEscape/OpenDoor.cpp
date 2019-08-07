0d64df12020f7844fcc5fe21fac0fc2f68c3fa52
Abin Alex Pothen <abinalexpothen@gmail.com>
2019-08-07 00:51:33 -0400

    Added door opening and closing timeline for door blueprint.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -31,38 +31,20 @@ void UOpenDoor::BeginPlay()
     }
 }
 
-void UOpenDoor::OpenDoor()
-{
-    // if (!Owner) { return; }
-    // Owner->SetActorRotation(FRotator(0.f, OpenAngle, 0.f));
-    OnOpenRequest.Broadcast();
-}
-
-void UOpenDoor::CloseDoor()
-{
-    if (!Owner) { return; }
-    Owner->SetActorRotation(FRotator(0.f, 0.f, 0.f));
-}
-
-
 // Called every frame
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// if the ActorThatOpens is in the trigger volume
-    if (GetTotalMassOfActorsOnPlate() > 50.f) // TODO: make into a parameter
+    if (GetTotalMassOfActorsOnPlate() > TriggerMass) // TODO: make into a parameter
     {
-        OpenDoor();
-        LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+        OnOpen.Broadcast();
     }
-    
-    // check if it's time to close the door
-    if (GetWorld()->GetTimeSeconds() > LastDoorOpenTime + DoorCloseDelay)
+    else
     {
-        CloseDoor();
+        OnClose.Broadcast();
     }
-
 }
 
 float UOpenDoor::GetTotalMassOfActorsOnPlate()
--- BuildingEscape/Source/BuildingEscape/OpenDoor.h
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -7,7 +7,7 @@
 #include "Engine/TriggerVolume.h"
 #include "OpenDoor.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnOpenRequest);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FDoorEvent);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
@@ -26,23 +26,17 @@ public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
     
-    void OpenDoor();
-    void CloseDoor();
+    UPROPERTY(BlueprintAssignable)
+    FDoorEvent OnOpen;
     
     UPROPERTY(BlueprintAssignable)
-    FOnOpenRequest OnOpenRequest;
+    FDoorEvent OnClose;
 
 private:
     UPROPERTY(EditAnywhere)
-    float OpenAngle = -90.0f;
-    
-    UPROPERTY(EditAnywhere)
     ATriggerVolume* PressurePlate = nullptr;
     
-    UPROPERTY(EditAnywhere)
-    float DoorCloseDelay = 2.f;
-    
-    float LastDoorOpenTime;
+    float TriggerMass = 50.f;
 
     AActor* Owner = nullptr;
     
